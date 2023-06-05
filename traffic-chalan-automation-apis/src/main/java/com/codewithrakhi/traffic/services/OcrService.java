231ebf87f54bf45b02ccc99f8b464600c7be429f
RakhiKumari01 <btech60001.20@bitmesra.ac.in>
2023-06-06 00:26:07 +0530

    removed traffic violation folder

--- traffic-chalan-automation-apis/src/main/java/com/codewithrakhi/traffic/services/OcrService.java
+++
@@ -1,8 +0,0 @@
-package com.codewithrakhi.traffic.services;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface OcrService {
-
-    String readImageText(MultipartFile file);
-}
